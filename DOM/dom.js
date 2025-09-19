b57aa9f985ed1754349ef90c5d391821890dd566
farzana-yeasmin-etuu <farzanayeasmin111201@gmail.com>
2025-09-19 13:15:43 +0600

    dom practice

--- DOM/dom.js
+++ DOM/dom.js
@@ -34,4 +34,66 @@ const div5=document.createElement("div")
 div5.innerHTML="<strong>this is inner Html</strong>"
 body.append(div5)
 
+//html tags also can be added this way
+const italic=document.createElement("i")
+italic.innerHTML="this is an italic sentence"
+body.append(italic)
+
+
+//remove any element through id
+const rem=document.querySelector("#rem")
+rem.remove()
+div.append(rem)
+
+//remove through removeChild()
+div.removeChild(rem)
+div.append(rem)
+
+//getAttribute
+const div6=document.querySelector("div")
+const pget=document.querySelector("#get")
+console.log(pget.getAttribute("id"))
+//alternate of get attribute when available as a method
+console.log(pget.id)
+console.log(pget.title)
+
+const set=document.querySelector(".set")
+console.log(set.getAttribute("title"))
+console.log(set.getAttribute("class"))
+console.log(set.title)
+//set attribute
+console.log(set.setAttribute("title","no-title"))
+console.log(set.setAttribute("class","changed-class"))
+set.removeAttribute('class')
+
+//dataset
+console.log(pget.dataset)
+console.log(pget.dataset.test)
+console.log(pget.dataset.longerNameChange)
+pget.dataset.newName="new data set"
+console.log(pget.dataset.newName)
+
+//class list
+pget.classList.add("new-class")
+pget.classList.remove("cl2")
+//toggle remove the attribute if it exist and add if doesn't exist
+
+pget.classList.toggle("cl4") //add cl4 as it doesn't exist
+pget.classList.toggle("cl1") //remove cl1 as it exists
+//automatically add or remove if a bool value is passed
+pget.classList.toggle("cl5",true) //add
+pget.classList.toggle("cl6",true) //add
+pget.classList.toggle("new-class",false) //remove existing class name
+
+//style=> use css property
+pget.style.backgroundColor ="red"
+div.style.backgroundColor="blue"
+set.style.backgroundColor="green"
+italic.style.backgroundColor="orange"
+italic.style.fontSize= "50px"
+
+
+
+
+
 
