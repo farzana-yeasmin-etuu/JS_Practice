5029d1aed4a12126e327cca50bd62bc182480d61
farzana-yeasmin-etuu <farzanayeasmin111201@gmail.com>
2025-09-24 00:03:18 +0600

    class

---
+++ class & object/app.js
@@ -0,0 +1,10 @@
+//create a object
+const student = {
+    fullName:"Farzana",
+    marks: 25,
+    printmarks: function ()
+    {
+        console.log(this.marks); //marks ca be access through 'this' keyword
+    },
+
+};
